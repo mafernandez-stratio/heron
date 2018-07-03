5607a0d3ccd641209c9048520c8516c996540c9e
mafernandez-stratio <mafernandez@stratio.com>
2018-07-03 13:57:51 +0200

    Dummy Server with plain text

--- src/main/scala/heron/server/DummyServer.scala
+++ src/main/scala/heron/server/DummyServer.scala
@@ -3,24 +3,23 @@ package heron.server
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
-import akka.http.scaladsl.marshallers.sprayjson._
+import akka.http.scaladsl.marshalling.{Marshaller, ToResponseMarshaller}
+import akka.http.scaladsl.model.HttpEntity.ChunkStreamPart
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
-import spray.json.DefaultJsonProtocol
 
 import scala.concurrent.duration._
 import scala.io.StdIn
 
-case class Example(id: Long, txt: String, number: Double)
-
-object MyJsonProtocol extends SprayJsonSupport with DefaultJsonProtocol {
-
-  implicit val exampleFormat = jsonFormat3(Test)
+case class Test(id: Long, txt: String, number: Double) {
+  override def toString: String = s"$id,$txt,$number"
 }
 
-class BatchIterator(batchSize: Int, numberOfBatches: Int, pause: FiniteDuration) extends Iterator[Array[Test]]{
+class PausedIterator(batchSize: Int, numberOfBatches: Int, pause: FiniteDuration) extends Iterator[Array[Test]]{
+
+  var iter = 1
 
   val range = Range(0, batchSize*numberOfBatches).toIterator
   val numberOfBatchesIter = Range(0, numberOfBatches).toIterator
@@ -28,9 +27,11 @@ class BatchIterator(batchSize: Int, numberOfBatches: Int, pause: FiniteDuration)
   override def hasNext: Boolean = range.hasNext
 
   override def next(): Array[Test] = {
+    println(s"Iteration $iter/$numberOfBatches")
     println(s"Sleeping for ${pause.toMillis} ms")
     Thread.sleep(pause.toMillis)
     println(s"Taking $batchSize elements")
+    iter = iter+1
     Range(0, batchSize).map{ _ =>
       val count = range.next()
       Test(count, s"Text$count", count*0.5)
@@ -38,13 +39,16 @@ class BatchIterator(batchSize: Int, numberOfBatches: Int, pause: FiniteDuration)
   }
 }
 
-object Server extends App {
-  import MyJsonProtocol._
-  implicit val jsonStreamingSupport: JsonEntityStreamingSupport = EntityStreamingSupport.json()
-
+object DummyServer extends App {
   implicit val system = ActorSystem("api")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
+
+  implicit val toResponseMarshaller: ToResponseMarshaller[Source[Array[Test], Any]] =
+    Marshaller.opaque { items =>
+      val data = items.map(item => ChunkStreamPart(s"${item.mkString("|")}${System.lineSeparator}"))
+      HttpResponse(entity = HttpEntity.Chunked(ContentTypes.`text/plain(UTF-8)`, data))
+    }
 
   def fetchExamples(): Source[Array[Test], NotUsed] = Source.fromIterator(() => new BatchIterator(5, 5, 2 seconds))
 
@@ -54,7 +58,7 @@ object Server extends App {
     }
 
   val bindingFuture = Http().bindAndHandle(route, "localhost", 9090)
-  println("Server started at localhost:9090")
+  println("Dummy Server started at localhost:9090")
   StdIn.readLine()
   bindingFuture.flatMap(_.unbind()).onComplete(_ ⇒ system.terminate())
 }
