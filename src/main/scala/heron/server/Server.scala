a678b3343ef70d3a2120ae03e1c6c5651c16cc04
mafernandez-stratio <mafernandez@stratio.com>
2018-07-03 15:06:20 +0200

    Improvement

--- src/main/scala/heron/server/Server.scala
+++ src/main/scala/heron/server/Server.scala
@@ -7,7 +7,8 @@ import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSup
 import akka.http.scaladsl.marshallers.sprayjson._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Flow, Source}
+import akka.util.ByteString
 import spray.json.DefaultJsonProtocol
 
 import scala.concurrent.duration._
@@ -25,12 +26,16 @@ class BatchIterator(batchSize: Int, numberOfBatches: Int, pause: FiniteDuration)
   val range = Range(0, batchSize*numberOfBatches).toIterator
   val numberOfBatchesIter = Range(0, numberOfBatches).toIterator
 
+  var iter = 1
+
   override def hasNext: Boolean = range.hasNext
 
   override def next(): Array[Test] = {
+    println(s"Iteration $iter/$numberOfBatches")
     println(s"Sleeping for ${pause.toMillis} ms")
     Thread.sleep(pause.toMillis)
     println(s"Taking $batchSize elements")
+    iter = iter+1
     Range(0, batchSize).map{ _ =>
       val count = range.next()
       Test(count, s"Text$count", count*0.5)
@@ -41,12 +46,15 @@ class BatchIterator(batchSize: Int, numberOfBatches: Int, pause: FiniteDuration)
 object Server extends App {
   import MyJsonProtocol._
   implicit val jsonStreamingSupport: JsonEntityStreamingSupport = EntityStreamingSupport.json()
+    .withFramingRenderer(
+      Flow[ByteString].intersperse(ByteString(System.lineSeparator))
+    )
 
   implicit val system = ActorSystem("api")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
-  def fetchExamples(): Source[Array[Test], NotUsed] = Source.fromIterator(() => new BatchIterator(5, 5, 2 seconds))
+  def fetchExamples(): Source[Array[Test], NotUsed] = Source.fromIterator(() => new BatchIterator(5, 50, 2 seconds))
 
   val route =
     path("example") {
